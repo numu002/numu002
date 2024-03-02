f6c2587dc4395da83fd0e09779969ef970491c77
numu002 <Leeyaoxuan@sina.com>
2024-03-02 23:00:16 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
 createApp(App).use(store).use(router).mount('#app')
